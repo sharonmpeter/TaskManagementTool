80a647059467c3326504e973e12a47249f3922a5
Sharon Peter <sharonpeter@Sharons-MacBook-Pro.local>
2023-02-16 20:49:43 -0500

    screens binding

--- dashboardadmin.js
+++ dashboardadmin.js
@@ -1,3 +1,19 @@
+var data = [
+    {
+      "UserID": 1,
+      "UserName": "rooter",
+      "Password": "12345",
+      "Country": "UK",
+      "Email": "sac@gmail.com"
+    },
+    {
+      "UserID": 2,
+      "UserName": "binu",
+      "Password": "123",
+      "Country": "uk",
+      "Email": "Binu@gmail.com"
+    }
+];
 function addtask(){
    // window.alert("hello");
     var e=document.getElementById("add");
@@ -30,3 +46,25 @@ function onedit(task){
    // task.parentNode.removeChild(listItem);
   
 }
+
+// tbl()
+$(document).ready(
+    function tbl() {
+    var html = '<table class="table table-striped">';
+    html += '<tr>';
+    var flag = 0;
+    $.each(data[0], function(index, value){
+        html += '<th>'+index+'</th>';
+    });
+    html += '</tr>';
+     $.each(data, function(index, value){
+         html += '<tr>';
+        $.each(value, function(index2, value2){
+            html += '<td>'+value2+'</td>';
+        });
+        html += '<tr>';
+     });
+     html += '</table>';
+     $('body').html(html);
+}
+);
--- login.js
+++ login.js
@@ -1,16 +1,44 @@
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
+var admin = document.getElementById("admin");
+var user = document.getElementById("user");
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+//data
+const email = document.getElementById("email");
+const pass = document.getElementById("password");
+
+// loginButton.addEventListener("click", (e) => {
+//    // e.preventDefault();
+//     const username = loginForm.username.value;
+//     const password = loginForm.password.value;
+
+//     if (username === "user" && password === "123") {
+//         alert("You have successfully logged in.");
+//     } else {
+//         loginErrorMsg.style.opacity = 1;
+//     }
+// })
+
+function loginClick(){
+    // const username = email.value;
+    //     const pass = password.value;
+        // window.location.href="dashboard.html";
+        console.log(email.value );
+        console.log(pass.value);
+        if (email.value == "user" && pass.value == "123") {
+            if (admin.checked == true){
+                window.location.href="dashboardadmin.html";
+            } else {
+                window.location.href="dashboard.html";
+            }
+            
+            
+            
+        } else {
+            // loginErrorMsg.style.opacity = 1;
+            console.log("error");
+        }
+}
